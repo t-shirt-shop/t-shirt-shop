b1672531c435250a8fd9fca95e6c65b14f0d3130
BryceEvans <evans.bryce@gmail.com>
2019-07-01 14:41:56 -0600

    less tested.

---
+++ front_end/tshirts/src/components/landingpage/StyledLabel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class StyledLabel extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    hi!
+                </p>
+            </div>
+        )
+    }
+}
+
+export default StyledLabel;
