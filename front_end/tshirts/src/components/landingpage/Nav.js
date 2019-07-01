19bc62a917f9b6375b5e58ff166748c3ebfeb673
Leslie <rleslie1015@gmail.com>
2019-07-01 15:49:46 -0500

    nav halfway done just needs logo and styling

--- front_end/tshirts/src/components/landingpage/Nav.js
+++ front_end/tshirts/src/components/landingpage/Nav.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
+// const Div = styled.div`
+//     display: flex;
+// ` 
 
 function Nav() {
     return (
         <div>
-            
+            <p>Logo</p>
+            <div> <FontAwesomeIcon icon={faShoppingCart} /> </div>  
             <div> <FontAwesomeIcon icon={faUser} /> </div>  
 
         </div>
