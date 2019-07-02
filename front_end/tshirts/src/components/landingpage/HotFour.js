cf98f820ce2c0ba7efa2b4f30982e798a09587d0
Leslie <rleslie1015@gmail.com>
2019-07-01 21:04:50 -0500

    hot four banner

---
+++ front_end/tshirts/src/components/landingpage/HotFour.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+    padding-top: 15px; 
+    color: white;
+    margin: 0 auto;
+`
+const P = styled.p`
+    padding-bottom: 15px; 
+    color: white;
+    margin: 0 auto;
+`
+const Div = styled.div`
+    background-image: linear-gradient(to right, #FF7676, #FFAE4E);
+`
+const HotFour = (props) => {
+
+    return (
+        <Div>
+            <H1>Hot Four</H1>
+            <P>POPULAR ITEMS THIS WEEK</P>
+        </Div>
+    )
+}
+
+export default HotFour;
--- front_end/tshirts/src/components/landingpage/Landing.js
+++ front_end/tshirts/src/components/landingpage/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "./Nav";
 import SaleThumbnailSection from "../../components/landingpage/ShirtThumbnailGrid.js/SaleThumbnailSection";
 import Hero from "./Hero";
+import HotFour from "./HotFour";
 
 class Landing extends React.Component {
     constructor() {
@@ -16,6 +17,9 @@ class Landing extends React.Component {
 
                 {/* Leslie, I'll put my items below this line for now. */}
                 <SaleThumbnailSection />
+
+                {/*HOT FOUR BANNER NOT THE GRID THAT IS STILL ALL YOU BRYCE :)*/}
+                <HotFour />
             </div>
         )
     }
