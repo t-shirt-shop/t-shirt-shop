2d4dd588610ed949b996742ae778d18394cd069b
Leslie <rleslie1015@gmail.com>
2019-07-01 20:51:09 -0500

    more styling

--- front_end/tshirts/src/components/landingpage/ShirtThumbnailGrid.js/StyledLabel.js
+++ front_end/tshirts/src/components/landingpage/ShirtThumbnailGrid.js/StyledLabel.js
@@ -18,14 +18,28 @@
 // export default StyledLabel;
 
 import React from 'react';
+import styled from 'styled-components';
 
+const H1 = styled.h1`
+    padding-top: 15px; 
+    color: white;
+    margin: 0 auto;
+`
+const P = styled.p`
+    padding-bottom: 15px; 
+    color: white;
+    margin: 0 auto;
+`
+const Div = styled.div`
+    background-image: linear-gradient(to right, #9F6EA3, #FF74A4);
+`
 const StyledLabel = (props) => {
 
     return (
-        <div>
-            <h1>24 Hour Sale</h1>
-            <p>15% OFF SELECT ITEMS</p>
-        </div>
+        <Div>
+            <H1>24 Hour Sale</H1>
+            <P>15% OFF SELECT ITEMS</P>
+        </Div>
     )
 }
 
