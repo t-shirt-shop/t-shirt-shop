03891a65ec16a8fdc155ec286fd3337eddc06f6d
lanners-marshall <lanners.marshall@gmail.com>
2019-07-02 11:44:53 -0700

    update

--- front_end/tshirts/src/store/reducers/usersReducer.js
+++ front_end/tshirts/src/store/reducers/usersReducer.js
@@ -57,14 +57,10 @@ const usersReducer = (state = initialState, action) => {
                   loginError: '',
                   user: action.payload
               }
-<<<<<<< HEAD
+
           default:
             return state
-=======
-            default:
-                return state;
->>>>>>> 09a8454fb63025d60b07a774847deb5407657be4
-      }
+          }
 }
 
 export default usersReducer;
