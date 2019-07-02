1ed506ffbf486e5fa5ce2658ef63d53ed7fa237e
Leslie <rleslie1015@gmail.com>
2019-07-02 14:44:54 -0500

    fixed button

--- front_end/tshirts/src/components/landingpage/ShirtThumbnailGrid.js/RoundedButton.js
+++ front_end/tshirts/src/components/landingpage/ShirtThumbnailGrid.js/RoundedButton.js
@@ -1,55 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 
-const SalesButton = styled.div`
-  width: 92%;
-  background-image: linear-gradient(to right, #9F6EA3, #FF74A4);
-  margin: auto;
-  margin-top: 15px;
-  position: relative;
-  height: 50px;
-  border-radius: 50px;
+const StyledButton = styled.button`
+background-image: linear-gradient(to right, #9F6EA3, #FF74A4); 
+display: inline-block;
+border: none;
+padding: 1rem 2rem;
+margin: 15px 0;
+text-decoration: none;
+color: #ffffff;
+font-family: sans-serif;
+font-size: 1rem;
+cursor: pointer;
+text-align: center;
+transition: background 250ms ease-in-out, 
+            transform 150ms ease;
+-webkit-appearance: none;
+-moz-appearance: none;
+width: 330px;
+border-radius: 30px;
+font-family: arial;
+font-weight: bold;
 `
-
+const Span = styled.span`
+    float: right;
+`
 const P = styled.p`
-    /* padding-bottom: 20px;  */
-    color: white;
-    margin: 0 auto;
-    font-family: arial;
-    font-size: 2vh;
-    font-weight: bold;
-    /* line-height: 3.5vh; */
-    position: absolute;
-    position: relative;
-    top: 40%;
-    padding: 0 10px;
+  padding-bottom: 15px; 
+  margin: 0 auto;
+  font-family: arial;
+  font-style: italic;
+  font-size: 2vh;
+  font-weight: bold;
+  line-height: 4vh;
+  align-content: center;
 `
 
-const ArrowContainer = styled.div`
-    background-color: green;
-    width: 40px;
-    height: 40px;
-    border-radius: 50px;
-    /* position: absolute;
-    position: relative; */
-    /* top: 50%; */
-    transform: rotate(90deg);
-    /* margin-left: 20px; */
-`
+
 
 const RoundedButton = (props) => {
   return (
       <div>
-        <SalesButton>
-          <P>
-            SHOP SALES
-          </P>
-          <ArrowContainer>
-            <FontAwesomeIcon icon={faChevronUp} style={{color: "white", textAlign: "center", border: "1px solid red"}}/> 
-          </ArrowContainer>
-        </SalesButton>
+          <StyledButton>SHOP SALES
+            <Span><FontAwesomeIcon icon={faChevronCircleRight} size="xl"/></Span> 
+        </StyledButton> 
       </div>
   )
 }
