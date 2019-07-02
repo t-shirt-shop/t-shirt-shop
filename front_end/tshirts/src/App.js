3950db3b1379bd421f8232f22166b64f40409884
Leslie <rleslie1015@gmail.com>
2019-07-02 16:00:45 -0500

    building out components

--- front_end/tshirts/src/App.js
+++ front_end/tshirts/src/App.js
@@ -3,12 +3,16 @@ import Landing from "./components/landingpage/Landing";
 import Shirts from "./components/shirts/Shirts";
 import './App.css';
 import { Route } from "react-router-dom";
+import Login from './components/registerLogin/Login';
+import Register from './components/registerLogin/Register';
 
 function App() {
   return (
     <div className="App">
     	<Route exact path="/" component={Landing} />
     	<Route path="/shirts" component={Shirts} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </div>
   );
 }
---
+++ front_end/tshirts/src/components/registerLogin/Register.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { connect } from "react-redux";
+import { registerNewUser } from '../../store/actions/usersActions';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const Button = styled.button`
+    background-image: linear-gradient(to right, #2FC145, #B4FF4E); 
+    display: inline-block;
+    border: none;
+    padding: 1rem 2rem;
+    margin: 0;
+    text-decoration: none;
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out, 
+                transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background: #40C947;
+    width: 330px;
+    border-radius: 30px;
+    font-family: arial;
+    font-weight: bold;
+`
+const ContainerDiv = styled.div`
+    background: #F2F8F8;
+    margin: 25px 0;
+`
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid #515C6F;
+
+`;
+const H2 = styled.h2`
+    padding-top: 40px; 
+    margin: 0 auto;
+    font-family: arial;
+    font-weight: 700;
+    font-size: 4.7vh;
+`
+const P = styled.p`
+    padding-bottom: 15px; 
+    margin: 0 auto;
+    font-family: arial;
+    font-style: italic;
+    font-size: 2vh;
+    font-weight: bold;
+    line-height: 4vh;
+    align-content: center;
+`
+const Span = styled.span`
+    float: right;
+`
+
+class NewUsers extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            registerUser: {
+            name: "",
+            email: "",
+            password: ""
+            }
+        };
+    }
+
+    handleChange = e => {
+        this.setState({ 
+            registerUser: {
+                ...this.state.registerUser,
+                [e.target.name]: e.target.value
+            }
+        })
+      };
+
+    handleSubmit = e => {
+        console.log("should be routing!!!!!!!!!")
+        console.log(this.props)
+        e.preventDefault();
+        this.props.registerNewUser(this.state.registerUser)
+        .then(() => {
+            this.props.history.push("/shirts");
+         });
+        this.setState({ 
+            registerUser: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        })
+    };
+
+    render(){
+        return(
+            <ContainerDiv>
+                <H2>FOR NEW USERS ONLY</H2>
+                <P>ENJOY AND ADDITIONAL 10% OFF YOUR ORDER <br/> WHEN YOU REGISTER TODAY</P>
+
+                <form onSubmit={this.handleSubmit}>
+                    <Input 
+                        type="text"
+                        placeholder="Theodor Seuss Geisel"
+                        value={this.state.name} 
+                        onChange={this.handleChange}
+                        name='name'
+                    />
+                    <Input 
+                        type="text"
+                        placeholder="TheoderSeuss@gmail.com"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        name='email'
+                    />
+                    <Input 
+                        placeholder="1Fish2Fish"
+                        type="text"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        name='password'
+                    />
+                </form>
+                <Button onClick={this.handleSubmit}>SIGN UP <Span>
+                {/* <img src={arrow} */}
+                    <FontAwesomeIcon icon={faChevronCircleRight} size="xl"/>
+                    </Span> </Button>
+            </ContainerDiv>
+        );
+    }
+}
+
+export default connect(null, { registerNewUser })(NewUsers);
