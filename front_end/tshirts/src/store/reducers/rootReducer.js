0f3ae31adaa4e3f6b3c3d4398430f3a0b9420745
lanners-marshall <lanners.marshall@gmail.com>
2019-07-01 21:36:19 -0700

    redux setup

---
+++ front_end/tshirts/src/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import cartsReducer from "./cartsReducer";
+import ordersReducer from './ordersReducer';
+import shirtsReducer from './shirtsReducer';
+import usersReducer from './usersReducer';
+
+const rootReducer = combineReducers({
+  cart: cartsReducer,
+  orders: ordersReducer,
+  shirts: shirtsReducer,
+  users: usersReducer
+});
+
+export default rootReducer;
