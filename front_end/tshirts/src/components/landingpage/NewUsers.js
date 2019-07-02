1463b21dc5432c691381086a0e5f7e7f72900c11
Leslie <rleslie1015@gmail.com>
2019-07-02 17:37:40 -0500

    props history bug

--- front_end/tshirts/src/components/landingpage/NewUsers.js
+++ front_end/tshirts/src/components/landingpage/NewUsers.js
@@ -92,12 +92,11 @@ class NewUsers extends React.Component {
         console.log(this.props)
         e.preventDefault();
         this.props.registerNewUser(this.state.registerUser)
-        .then(() => {
-            this.props.history.push("/shirts");
-         });
+        this.props.history.push("/shirts");
+       
         this.setState({ 
             registerUser: {
-                name: '',
+                "user_name": '',
                 email: '',
                 password: ''
             }
@@ -114,9 +113,9 @@ class NewUsers extends React.Component {
                     <Input 
                         type="text"
                         placeholder="Theodor Seuss Geisel"
-                        value={this.state.name} 
+                        value={this.state.user_name} 
                         onChange={this.handleChange}
-                        name='name'
+                        name='user_name'
                     />
                     <Input 
                         type="text"
