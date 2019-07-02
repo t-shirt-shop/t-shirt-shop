c80d8353ca77cd61a75b79721f04f9cccc448071
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-07-02 15:54:54 -0500

    Starting product page -Added route in app.js -built out functional component with <Nav /> in it.
    Next:
    Build out the t-shirt image, circle price button, custimization sections

--- front_end/tshirts/src/App.js
+++ front_end/tshirts/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Landing from "./components/landingpage/Landing";
 import Shirts from "./components/shirts/Shirts";
+import Product from "./components/product/Product";
 import './App.css';
 import { Route } from "react-router-dom";
 
@@ -9,6 +10,8 @@ function App() {
     <div className="App">
     	<Route exact path="/" component={Landing} />
     	<Route path="/shirts" component={Shirts} />
+    	<Route path="/product" component={Product} /> 
+      {/* ^^ Will change to the exact shirt name or products/shirt_name after skeleton is built  */}
     </div>
   );
 }
---
+++ front_end/tshirts/src/components/product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../landingpage/Nav';
+import Hero from '../landingpage/Hero';
+
+const Product = (	) => {
+	return ( <>
+	<Nav />
+	</> );
+}
+ 
+export default Product;
