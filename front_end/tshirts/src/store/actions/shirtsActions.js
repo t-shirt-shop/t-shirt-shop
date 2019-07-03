bd40de430dfdebe75032e308bf3fe20e9f1626fd
lanners-marshall <lanners.marshall@gmail.com>
2019-07-02 17:53:57 -0700

    update

--- front_end/tshirts/src/store/actions/shirtsActions.js
+++ front_end/tshirts/src/store/actions/shirtsActions.js
@@ -12,3 +12,16 @@ export const shirtsByCategory = (id) => {
 		})
 	}
 }
+
+export const getAllCatagories = () => {
+	return dispatch => {
+		dispatch({type: 'GET_CATEGORY_START'})
+		axios.get(`https://t-shirt-store123.herokuapp.com/category`)
+		.then(res => {
+			dispatch({type: 'GET_CATEGORY_SUCCESS', payload: res.data})
+		})
+		.catch(error => {
+			dispatch({type: 'GET_CATEGORY_ERROR', payload: error})
+		})
+	}
+}
--- front_end/tshirts/src/store/reducers/shirtsReducer.js
+++ front_end/tshirts/src/store/reducers/shirtsReducer.js
@@ -1,18 +1,29 @@
 const initialState = {
-	loading: false,
+	loading_shirts: false,
+	loading_categories: false,
 	error: null,
-	shirts: []
+	shirts: [],
+	catagories: []
 }
 
 const shirtsReducer = (state = initialState, action) => {
 	let data = action.payload
+
 	switch (action.type){
 		case 'GET_BY_CATEGORY_START':
-			return {...state, loading: true, error: null}
+			return {...state, loading_shirts: true, error: null}
 		case 'GET_BY_CATEGORY_SUCCESS':
-			return {...state, loading: false, error: null, shirts: data}
+			return {...state, loading_shirts: false, error: null, shirts: data}
 		case 'GET_BY_CATEGORY_ERROR':
-			return {...state, loading: false, error: null, error: data}
+			return {...state, loading_shirts: false, error: null, error: data}
+
+		case 'GET_CATEGORY_START':
+			return {...state, loading_categories: true, error: null}
+		case 'GET_CATEGORY_SUCCESS':
+			return {...state, loading_categories: false, error:null, catagories: data}
+		case 'GET_CATEGORY_ERROR':
+			return {...state, loading_categories: false, error: data}
+
 		default:
 			return state
 	}
