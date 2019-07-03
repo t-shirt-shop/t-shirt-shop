3b504f2be61024437228606d8b597d4886c888e6
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-07-03 13:11:11 -0500

    Quantity counter working. -gonna move to like button then selecting size/color. I'll probably just do a green border like the orignal design for time.

--- front_end/tshirts/src/components/product/components/Quantity.js
+++ front_end/tshirts/src/components/product/components/Quantity.js
@@ -1,45 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React,{Component} from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-display: flex;
-align-items: center;
-justify-content: flex-end;
-margin-right: 43px;
-margin-top: 30px;
-
-`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 43px;
+  margin-top: 30px;
+`;
 const SubtractButton = styled.div`
-width: 26px;
-height: 26px;
-margin: 10px;
-background-color: rgba(114, 124, 142, .20);
-border-radius: 100%;
-font-weight: bolder;
-`
+  width: 26px;
+  height: 26px;
+  margin: 10px;
+  background-color: rgba(114, 124, 142, 0.2);
+  border-radius: 100%;
+  font-weight: bolder;
+`;
 const AddButton = styled.div`
-width: 26px;
-height: 26px;
-margin: 10px;
-background-color: rgba(114, 124, 142, .20);
-border-radius: 100%;
-font-weight: bolder;
+  width: 26px;
+  height: 26px;
+  margin: 10px;
+  background-color: rgba(114, 124, 142, 0.2);
+  border-radius: 100%;
+  font-weight: bolder;
+`;
 
-`
+class Quantity extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    };
+  }
+  countUp = count => {
+    this.setState({count: count+=1});
+  };
+	countDown = count => 
+	{
+		if(this.state.count != 0) {
 
-let count = 1;
+			this.setState({count: count-=1});
+		}
+  };
 
-const countUp = (count) => {return count++};  
-const countDown = (count) => {return count--}; 
-const Quantity = () => {
-	return ( <Container>
-		<p>
-			Quantity
-		</p>
-		<SubtractButton onClick={countDown(count)}>-</SubtractButton>
-		{count}
-		<AddButton onClick={()=>countUp(count)}>+</AddButton>
-	</Container> );
+  render() {
+    return (
+      <Container>
+        <p>Quantity</p>
+        <SubtractButton onClick={()=>this.countDown(this.state.count)}>
+          -
+        </SubtractButton>
+        {this.state.count}
+        <AddButton onClick={()=>this.countUp(this.state.count)}>+</AddButton>
+      </Container>
+    );
+  }
 }
- 
 export default Quantity;
